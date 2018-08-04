961bde61f6e8200f9075b16c612af44118d4bdf6
illusion_ <thortwo@outlook.com>
2018-08-04 15:15:45 +0800

    test

--- Api_News/spiders/ApiNews.py
+++ Api_News/spiders/ApiNews.py
@@ -10,12 +10,12 @@ class ApinewsSpider(scrapy.Spider):
     allowed_domains = ['47.90.63.143']
     api = 'OrTqAiyVROsmXuQMR6Lmr7eENi5GZX7o6swyVh1KcHHu7nGccWjTgLEBWW7WtqVD'
     start_urls = [
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=0&catid=10028&apikey=%s' % api,
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=20&catid=10028&apikey=%s' % api,
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=40&catid=10028&apikey=%s' % api,
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=60&catid=10028&apikey=%s' % api,
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=80&catid=10028&apikey=%s' % api,
-        'https://47.90.63.143/news/appledailyhk?type=3&pageToken=100&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=0&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=20&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=40&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=60&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=80&catid=10028&apikey=%s' % api,
+        'https://47.90.63.143/news/appledailyhk?type=2&pageToken=100&catid=10028&apikey=%s' % api,
     ]
 
     def parse(self, response):
@@ -25,7 +25,7 @@ class ApinewsSpider(scrapy.Spider):
         hasNext = result['hasNext']
         print(hasNext)
         print(SnowNLP(result['data'][0]['title']))
-        item['dataType'] = 'meishi'
+        item['dataType'] = 'yule'
         item['author'] = result['appCode']
         for news in result['data']:
             if not news['videoUrls']:
